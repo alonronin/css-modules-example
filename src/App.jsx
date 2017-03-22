397cee0131640f0e6a5bc7244ebe535200754aef
Alon Valadji <alon@ronin.co.il>
2017-03-22 23:02:28 +0800

    stupid reformatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { header, big } from "./style";
+import { big, header } from "./style";
 import Component from "./Component";
 
 export default function App() {
@@ -7,10 +7,11 @@ export default function App() {
     <div>
       <h1 className={header}><span>Hello</span></h1>
 
-      <p className={big}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab architecto blanditiis consectetur
-        consequuntur
+      <p className={big}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab architecto blanditiis consectetur consequuntur
         delectus, fuga hic iusto laudantium magni odio odit possimus provident, quis quod repudiandae sunt tempore,
-        voluptas voluptatibus.</p>
+        voluptas voluptatibus.
+      </p>
 
       <Component />
     </div>
--- src/Component.jsx
+++ src/Component.jsx
@@ -3,9 +3,10 @@ import styles from "./component.css";
 
 export default function Component() {
   return (
-    <p className={styles.big}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis placeat quaerat sequi
-      sint. Cumque labore,
+    <p className={styles.big}>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis placeat quaerat sequi sint. Cumque labore,
       voluptates! Amet aut, autem consequuntur, corporis doloremque facere, impedit minus praesentium quibusdam
-      reprehenderit sunt vitae.</p>
+      reprehenderit sunt vitae.
+    </p>
   )
 }
